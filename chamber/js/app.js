fb6f85d3a1e7193024b1e136bdf3f7214cad70c2
HerreraAxelBYU <97065752+HerreraAxelBYU@users.noreply.github.com>
2022-05-15 01:42:39 -0300

    Update app.js

--- chamber/js/app.js
+++ chamber/js/app.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector('#hamburger-button');
 const nav = document.querySelector('#primaryNav');
 const container_date = document.querySelector('.header__date');
-const today = new Date();
+const today = new Date(document.lastModified);
 const current_date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	today);
+const date = document.querySelector('.last__modification');
+
 
 function toggleMenu(){
     document.querySelector('#primaryNav').classList.toggle('open');
@@ -14,3 +16,5 @@ function toggleMenu(){
 hamburger.onclick = toggleMenu;
 container_date.innerHTML = `<p>${current_date}</p>`;
 
+
+date.textContent =`Last modification: ${document.lastModified}`;
