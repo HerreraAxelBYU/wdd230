99d0576bfdbd34e0973e8afa046b84c3922de041
HerreraAxelBYU <97065752+HerreraAxelBYU@users.noreply.github.com>
2022-05-14 21:23:44 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
-const years = new Date()
-const last = new Date(document.lastModified)
+const hamburger = document.querySelector('#hamburger-button');
+const nav = document.querySelector('#primaryNav');
+const container_date = document.querySelector('.header__date');
+const today = new Date(document.lastModified);
+const current_date = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	today);
+const date = document.querySelector('.last__modification');
 
-document.querySelector('.year').innerHTML = years.getFullYear()
-document.querySelector('.date').innerHTML = last
+
+function toggleMenu(){
+    document.querySelector('#primaryNav').classList.toggle('open');
+    document.querySelector('#hamburger-button').classList.toggle('open');
+    console.log('Soy el mejor');
+}
+
+hamburger.onclick = toggleMenu;
+container_date.innerHTML = `<p>${current_date}</p>`;
+
+
+date.textContent =`Last modification: ${document.lastModified}`;
