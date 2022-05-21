01ff062a10329abdefaf7404ba8c51f0ad796b1d
HerreraAxelBYU <97065752+HerreraAxelBYU@users.noreply.github.com>
2022-05-21 19:53:10 -0300

    Update app.js

--- chamber/js/app.js
+++ chamber/js/app.js
@@ -16,7 +16,7 @@ function toggleMenu(){
 }
 
 function showBanner() {
-    if (day === 0 && day === 1) {
+    if (day === 1 || day === 2) {
         banner.textContent = "Come join us for the chamber meet and greet Wednesday at 7:00 p.m."
     }
     else {
