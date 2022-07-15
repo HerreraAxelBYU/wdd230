3869ff6e93b4c035888592a878348593a71eeb00
HerreraAxelBYU <her21075@byui.edu>
2022-07-15 17:29:09 -0300

    actualizar json

--- final-project/js/temples.js
+++ final-project/js/temples.js
@@ -6,11 +6,15 @@ fetch(temples_url)
   })
   .then(function (jsonObject) {
     //console.table(jsonObject);  // temporary checking for valid response and data parsing
-    console.log(jsonObject)
+    //console.log(jsonObject)
     const temples = jsonObject['temples']
-    
-    const names = temples.name
-    console.log()
+
+    // Buenos Aires Temple Variables
+    buenos_aires_temple = temples[0]
+
+    console.log(buenos_aires_temple)
+   
   });
 
-  
+
+
